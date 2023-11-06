4ca42a4545a6288881d486524eba95b3b8f1c6c0
Ryuya Uetake <38969179+deruloxxx@users.noreply.github.com>
2023-11-06 20:48:51 +0800

    feat: add prettier

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,13 +1,17 @@
-import {FC} from "react";
-import clsx from "clsx";
+import { FC } from 'react'
+import clsx from 'clsx'
 
 type Props = {
   handleSlider: boolean
   setHandleSlider: () => void
 }
 
-export const Slider:FC<Props> = ({handleSlider, setHandleSlider}) => {
+export const Slider: FC<Props> = ({ handleSlider, setHandleSlider }) => {
   return (
-    <div onClick={setHandleSlider} className={clsx(handleSlider ? 'is-open' : 'is-closed', "spa-slider")} title={handleSlider ? 'Click to retract' : 'Click to extend'}  />
-  );
-};
+    <div
+      onClick={setHandleSlider}
+      className={clsx(handleSlider ? 'is-open' : 'is-closed', 'spa-slider')}
+      title={handleSlider ? 'Click to retract' : 'Click to extend'}
+    />
+  )
+}
