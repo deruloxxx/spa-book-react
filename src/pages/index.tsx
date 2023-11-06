382c1e2871a72ab272153cf7e4bd07f8f436f677
Ryuya Uetake <38969179+deruloxxx@users.noreply.github.com>
2023-11-06 20:59:45 +0800

    feat: set meta title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import { ChatSlider } from '@/features/ChatSlider'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <main>
-      <div id={'spa'}>
-        <ChatSlider />
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>SPA</title>
+      </Head>
+      <main>
+        <div id={'spa'}>
+          <ChatSlider />
+        </div>
+      </main>
+    </>
   )
 }
